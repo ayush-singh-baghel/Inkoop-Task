b322813f56b8261cfb8b8628704751446538f55b
Ayush Singh <ayurrajput07@gmail.com>
2024-12-17 21:06:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InkoopTask from './components/InkoopTask';
+
+function App() {
+  return (
+    <div className="App">
+    <InkoopTask/>
+    </div>
+  );
+}
+
+export default App;
